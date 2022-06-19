2a68e5db8039647a27ac4455b4742abfd7d878b8
Nikitka <keirin1203@gmail.com>
2022-06-19 12:16:15 +0300

    added .env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,23 @@ import { ChatsModule } from './chats/chats.module';
 import { Chat } from "./chats/chat.model";
 import { MessagesModule } from './messages/messages.module';
 import { Message } from "./messages/message.model";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import 'dotenv/config'
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'terminal-chat',
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [User, Chat, Message],
       synchronize: true,
       logging: true,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config'
+
+const PORT = process.env.SERVER_PORT
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(9000, () => {
-    console.log(`App started at http://localhost:9000`)
+  await app.listen(PORT, () => {
+    console.log(`App started at http://localhost:${PORT}`)
   });
 }
 bootstrap();
