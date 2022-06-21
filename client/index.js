496645aa550e2ffe1bab30037ab17d21ab92f551
Nikitka <keirin1203@gmail.com>
2022-06-21 19:52:07 +0300

    added cats command

--- client/index.js
+++ client/index.js
@@ -1,24 +1,32 @@
 let term = $('body').terminal(function (command) {
     const cmd = $.terminal.parse_command(command)
+    console.log(cmd)
+
     switch (cmd.name) {
         case 'connect':
             socket = io()
 
             socket.on('messageToClient', (message) => {
-                this.echo(message)
+                this.echo(message, {raw: true})
             })
+            break;
+
+        case 'cats':
+            const img = `<img src="https://placekitten.com/${cmd.args[0]}/${cmd.args[1]}">`
+            socket.emit('messageToServer', img)
             break;
         default:
             if (!socket) {
                 this.echo('Please, connect to server')
                 break
             }
-            console.log(cmd)
+
             const message = cmd.name+ " " + cmd.rest
             socket.emit('messageToServer', message)
             break;
     }
     },
+
     {
         greetings: greetings.innerHTML,
         completion: true
